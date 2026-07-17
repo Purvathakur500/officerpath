420eaf708222d051e30f67cd1a767960c1430311
Purva Thakur <purvathakur9041@gmail.com>
2026-07-17 12:14:33 +0530

    Fix Neon secrets configuration

--- database/db.py
+++ database/db.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import psycopg2
 
-
 def get_connection():
     return psycopg2.connect(
-        host=st.secrets["ep-dawn-haze-azpyerpb.c-3.ap-southeast-1.aws.neon.tech"],
-        database=st.secrets["neondb"],
-        user=st.secrets["neondb_owner"],
-        password=st.secrets["npg_k0Koqaen8dyw"],
-        port=st.secrets["5432"],
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"],
         sslmode="require"
     )
